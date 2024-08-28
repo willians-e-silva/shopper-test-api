803a2a88cad6f873ead12fad55d7eae139a31757
willians silva <willians.silva@essentia.com.br>
2024-08-28 09:42:19 -0300

    removed unused code

--- src/controllers/invoice/invoiceController.ts
+++ src/controllers/invoice/invoiceController.ts
@@ -1,4 +1,4 @@
-import e, { Request, Response } from "express";
+import { Request, Response } from "express";
 import { container } from "tsyringe";
 import { InvoiceUseCase } from "./invoiceUseCase";
 
