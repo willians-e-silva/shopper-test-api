be48bd652cc4e2ebb2e5ee11ddc518ccc6d81bf1
Will E. Silva <86208577+willians-e-silva@users.noreply.github.com>
2024-08-28 09:15:30 -0300

    Update App.ts

--- src/App.ts
+++ src/App.ts
@@ -1,7 +1,6 @@
 import express from 'express';
 import { router } from './routes/routes';
 
-// import { errorHandler } from './middlewares/errorHandler';
 
 export class App {
   public express: express.Application;
@@ -12,7 +11,6 @@ export class App {
 
   public async init() {
     try {
-      // Initialize database connections or other async tasks here
       await this.start();
     } catch (error) {
       console.error(error);
@@ -22,11 +20,9 @@ export class App {
   public async start(): Promise<void> {
     this.express.use("/public", express.static(__dirname + '/public'));
 
-    // Middleware para analisar o corpo da requisição
     this.express.use(express.json());
     this.express.use(express.urlencoded({ extended: true }));
 
     this.express.use(router);
-    // this.express.use(errorHandler);
   }
 }
